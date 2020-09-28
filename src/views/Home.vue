ad914a58e6c3f080f2db377fbcaf49d7d64af9ec
donovanrichardson <donovanspqa@gmail.com>
2020-09-28 19:02:15 -0400

    projects div

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
 <div>
-    <h1>Heading</h1>
-    <h2>Heading</h2>
-    <h3>Heading</h3>
-    <h4>Heading</h4>
-    <h5>Heading</h5>
-    <h6>Heading</h6>
-
-    <p>Home</p>
+    <h1>Donovan Richardson</h1>
+    <h2>Full Stack Engineer</h2>
+    <!-- Capitalization?? -->
+    <p>Lorem ipsum brandmor state ament, consectetur adipisicing elit. Nam necessitatibus voluptatem quam dolores amet porro fugiat. Illo quae voluptas, quidem provident magni fugiat reiciendis eum impedit quas dicta assumenda unde.</p>
+    <div id="projects" v-for="project in projects" :key="project.name">
+        <div class="project">
+            <h3>{{project.name}}</h3>
+            <p>{{project.description}}</p>
+            <p v-if="project.backend"><a :href="project.backend">Backend</a></p>
+            <p v-if="project.frontend"><a :href="project.frontend">Frontend</a></p>
+            <p v-if="project.live"><a :href="project.live">Live App</a></p>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -17,6 +22,22 @@ export default {
     components: {
 
     },
+    data: function () {
+        return {
+            projects: [
+                {
+                    name:"Deparch",
+                    description:"Train Departure Timetables",
+                    // backend:,
+                    // frontend:,
+                    live:"https://donovanrichardson.github.io/departures/lirr.html"
+                },
+            ]
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
