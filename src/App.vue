464d169c06c6f0f4b684392dd5a81a0a36a6cf24
donovanrichardson <donovanspqa@gmail.com>
2020-09-28 18:45:19 -0400

    size of headings

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
 <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <router-view/>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    
 </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+
     }
 }
 </script>
 
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+body{
+  text-align: center;
 }
+  h3{
+    font-size:1.2em
+  }
+  h5{
+    font-size:0.9em
+  }
+  h6{
+font-size:0.8em
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,25 @@
 <template>
+<div>
+    <h1>Heading</h1>
+    <h2>Heading</h2>
+    <h3>Heading</h3>
+    <h4>Heading</h4>
+    <h5>Heading</h5>
+    <h6>Heading</h6>
+
     <p>Home</p>
+</div>
 </template>
+
+<script>
+export default {
+    name: "Home",
+    components: {
+
+    },
+}
+</script>
+
+<style>
+
+</style>
