1c5bb5aac9d17e0c8dcb36da02f1eac9bf125425
donovanrichardson <donovanspqa@gmail.com>
2020-09-29 13:43:29 -0400

    added grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
 <div>
-    <div id="header"></div>
+    <div id="header">
+        <div id="header-text">
+            <h1 id="my-name">Donovan Richardson</h1>
+            <h2 id="subtitle">Full Stack Engineer</h2>
+            <!-- Capitalization?? -->
+        </div>
+    </div>
     <div id="content">
-        <h1>Donovan Richardson</h1>
-        <h2>Full Stack Engineer</h2>
-        <!-- Capitalization?? -->
         <p>Lorem ipsum brandmor state ament, consectetur adipisicing elit. Nam necessitatibus voluptatem quam dolores amet porro fugiat. Illo quae voluptas, quidem provident magni fugiat reiciendis eum impedit quas dicta assumenda unde.</p>
-        <div id="projects" v-for="project in projects" :key="project.name">
-            <div class="project">
+        <div id="projects">
+            <div class="project" v-for="project in projects" :key="project.name">
                 <h3>{{project.name}}</h3>
                 <p>{{project.description}}</p>
                 <p v-if="project.backend"><a :href="project.backend">Backend</a></p>
@@ -62,22 +65,39 @@ export default {
 
 <style>
 html {
-    background: blue;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family:sans-serif
 }
 
 body {
     color: white;
     margin: 0;
+    background: #5E51F5;
 }
 
 #header {
     height: 40vw;
     width: 100%;
     background: #0000007f;
+    display:flex;
+    justify-content: center;
+    align-items: center;
 }
 
+#my-name{
+    margin-top:0
+}
+
+#subtitle{
+    margin-bottom:0
+}
 #content{
     margin: 0 8px;
-    
 }
+
+#projects{
+    display: grid;
+    grid-template-columns:1fr 1fr;  
+}
+
 </style>
